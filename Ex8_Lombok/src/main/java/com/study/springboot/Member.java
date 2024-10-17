282c126da8f819ab212b8b21d8256fd830fe8eff
ad.seoil <ad.seoil0112@gmail.com>
2024-10-17 12:39:25 +0900

    Lombok+

--- Ex8_Lombok/src/main/java/com/study/springboot/Member.java
+++ Ex8_Lombok/src/main/java/com/study/springboot/Member.java
@@ -1,12 +1,18 @@
 package com.study.springboot;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Data
-@Getter
-@Setter
+//@ToString
+//@Getter
+//@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Member {
 	private String id;
 	private String name;
