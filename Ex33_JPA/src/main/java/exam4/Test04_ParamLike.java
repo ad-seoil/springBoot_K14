27efa8ead5e7ad8066fa77841592dc1d121b765d
ad.seoil <ad.seoil0112@gmail.com>
2024-11-06 10:14:29 +0900

    1106 JPA

--- Ex33_JPA/src/main/java/exam4/Test04_ParamLike.java
+++ Ex33_JPA/src/main/java/exam4/Test04_ParamLike.java
@@ -1,5 +1,43 @@
 package exam4;
 
-public class Test04_ParamLike {
+import java.util.List;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import jakarta.persistence.TypedQuery;
+
+public class Test04_ParamLike {
+	public static void main(String[] args) {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("JpaEx01");
+		EntityManager em = emf.createEntityManager();
+		
+		try {
+			em.getTransaction().begin();
+			
+			TypedQuery<Member4> query = em.createQuery("select m from Member4 m "
+													+ " where m.email like :email "
+													+ " and m.name like :name "
+													+ " order by m.name",
+													Member4.class).setParameter("email", "%test.com%")
+																  .setParameter("name", "%양%"); // 파라미터값 세팅
+			List<Member4> result = query.getResultList();
+			
+			em.getTransaction().commit();
+			
+			if (result.isEmpty()) {
+				System.out.println("사용자가 없습니다");
+			} else {
+				result.forEach(user -> System.out.printf(": %s : %s : %tY-%<tm-%<td : \n", 
+						user.getEmail(), user.getName(), user.getCreateDate()));
+			}
+			
+		} catch (Exception e) {
+			em.getTransaction().rollback();
+			throw e;
+		}
+		
+		em.close();
+		emf.close();
+	}
 }
