60849da3f5df92c78a3cb3d6367a9092d8ac9990
ad.seoil <ad.seoil0112@gmail.com>
2024-11-28 10:11:00 +0900

    20241128 springboot bbs

--- SpringBbs/src/main/java/com/example/simplebbs/article/ArticleService.java
+++ SpringBbs/src/main/java/com/example/simplebbs/article/ArticleService.java
@@ -1,6 +1,7 @@
 package com.example.simplebbs.article;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
@@ -17,8 +18,15 @@ public class ArticleService {
 	}
 	
 	public void writeArticle(String subject, String content, String author) {
-		Article article = new Article(subject, content, author);
+		Article article 
+		= new Article(subject, content, author);
 		articleMapper.insertArticle(article);
+	}
+
+	public Article getById(Long id) {
+		//리턴되는 객체가 null인경우 에러발생방지
+		Optional<Article> article = articleMapper.findById(id);
+		return article.orElseThrow();
 	}
 	
 }
